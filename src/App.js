2851297afc8cb21065cd8f03ce9ad3c4ade88736
Tramit Heng <hengtramit@gmail.com>
2022-12-04 16:51:54 +0700

    Implement Auth

--- src/App.js
+++ src/App.js
@@ -24,24 +24,20 @@ import React from 'react';
 import './App.css';
 
 import { Amplify } from 'aws-amplify';
-import {awsconfig} from './aws-exports'
-import { Authenticator } from '@aws-amplify/ui-react';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-function App() {
+function App({ signOut, user }) {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <div className="App">
-          <p>
-            Hey {user.username}, welcome to my channel, with auth!
-          </p>
-          <button onClick={signOut}>Sign out</button>
-        </div>
-      )}
-    </Authenticator>
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
